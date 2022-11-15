afa77aaa283d0a421905a28cde7807759cd645cd
Steph Hjartarson <stephanie.hja@gmail.com>
2022-11-15 19:45:34 +0000

    deployment commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -49,6 +50,7 @@ class PostDetail(View):
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
+            messages.success(request, 'Comment posted successfully!')
             comment.save()
         else:
             comment_form = CommentForm()
